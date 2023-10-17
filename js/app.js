84d392e7ca0d72413a739dc617cbe2ad4ee1d2fd
LucaLF-dev <laficaraluca@gmail.com>
2023-10-17 21:10:32 +0200

    Add - html title and style css

--- js/app.js
+++ js/app.js
@@ -2,14 +2,12 @@
 
 function getArrayOfRandomIntBetween(min, max, number) {
 	const arrayBombs = [];
-	
+
 	while (arrayBombs.length < number) {
-		
 		const n = getRandomIntInclusive(min, max);
-	
+
 		console.log(arrayBombs.includes(n));
 		if (arrayBombs.includes(n) !== true) {
-			
 			arrayBombs.push(n);
 		}
 	}
@@ -23,23 +21,18 @@ function getRandomIntInclusive(min, max) {
 	return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
 }
 
+const containerDOMElement = document.querySelector(".container");
+console.log(containerDOMElement);
 
-const containerDOMElement = document.querySelector(".container")
-console.log(containerDOMElement)
+const numberItemDOMElement = document.querySelectorAll(".number-item");
+console.log(numberItemDOMElement);
 
-const numberItemDOMElement = document.querySelectorAll('.number-item')
-console.log(numberItemDOMElement)
+const arrayNumber = [];
+const arrayRandomNumber = getArrayOfRandomIntBetween(1, 99, 5);
+console.log(arrayNumber, arrayRandomNumber);
 
+for (i = 0; i < 5; i++) {
+	const htmlItem = `<div class="number-item"> ${arrayRandomNumber[i]}</div> `;
 
-const arrayNumber = []
-const arrayRandomNumber = getArrayOfRandomIntBetween(1,99,5)
-console.log(arrayNumber,arrayRandomNumber)
-
-for (i = 0; i < 4; i++) {
-    const htmlItem = `<div class="number-item"> ${arrayRandomNumber[i]}</div> `;
-
-    containerDOMElement.innerHTML += htmlItem
+	containerDOMElement.innerHTML += htmlItem;
 }
-
-
-
